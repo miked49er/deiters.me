00334ac103f485fd91eb7ba75357a8c463ae69f7
Mike Deiters <miked49er@gmail.com>
2017-02-18 11:25:13 -0500

    Fixed html5 history api issues

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mike Deiters' });
+});
+
+router.get('/photographer', function(req, res, next) {
+  res.render('index', { title: 'Mike Deiters' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'Mike Deiters' });
+});
+
+router.get('/developer', function(req, res, next) {
+  res.render('index', { title: 'Mike Deiters' });
 });
 
 module.exports = router;
