beb59ead14138baea4f4e703f534c296084bbd9a
Mike Deiters <miked49er@gmail.com>
2017-03-01 18:55:51 -0500

    Built functionality to read in the json files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mike Deiters' });
+});
+
+router.get('/bakuro', function(req, res, next) {
+
+  fs.readFile('./lib/json/bakuro.json', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+router.get('/projects', function(req, res, next) {
+
+  fs.readFile('./lib/json/projects.json', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
 });
 
 router.get('/photographer', function(req, res, next) {
