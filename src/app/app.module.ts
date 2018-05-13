ab42909504613ac6954d41917b7931c7b51abe33
Mike Deiters <miked49er@gmail.com>
2018-05-13 17:37:23 -0400

    Imported routes, browser animation, and project feature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectFeatureComponent } from './components/project-feature/project-feature.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectFeatureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
