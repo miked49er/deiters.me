616eff90aff409cd737f75650dae95b871da44ff
Mike Deiters <miked49er@gmail.com>
2018-05-20 12:33:48 -0400

    Built project feature functionality

--- src/app/components/project-feature/project-feature.component.ts
+++ src/app/components/project-feature/project-feature.component.ts
@@ -13,15 +13,37 @@ import {
   templateUrl: './project-feature.component.html',
   styleUrls: ['./project-feature.component.scss'],
   animations: [
-    trigger('scrollAnimation', [
+    trigger('scrollBackground', [
       state('show', style({
         transform: "translateY(0)"
       })),
       state('hide', style({
         transform: "translateY(100%)"
       })),
-      transition('hide => show', animate('200ms ease-in')),
-      transition('show => hide', animate('200ms ease-out'))
+      transition('hide => show', animate('400ms ease-in')),
+      transition('show => hide', animate('400ms 600ms ease-out'))
+    ]),
+    trigger('scrollOverlay', [
+      state('show', style({
+        transform: "translateY(0)"
+      })),
+      state('hide', style({
+        transform: "translateY(100%)"
+      })),
+      transition('hide => show', animate('400ms 400ms ease-in')),
+      transition('show => hide', animate('400ms 400ms ease-out'))
+    ]),
+    trigger('scrollTitle', [
+      state('show', style({
+        transform: "translateX(0)",
+        opacity: 1
+      })),
+      state('hide', style({
+        transform: "translateX(10%)",
+        opacity: 0
+      })),
+      transition('hide => show', animate('400ms 800ms ease-in')),
+      transition('show => hide', animate('400ms ease-out'))
     ])
   ]
 })
