f4e85fd6c0205f775269df07da87b763cd1ea365
Mike Deiters <miked49er@gmail.com>
2018-06-10 12:17:53 -0400

    pull projects from project service

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'deiters-nav',
@@ -6,12 +7,30 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  @Input() project?: number;
+  project: number;
   @Input() ascii: boolean;
 
-  constructor() { }
+  constructor(
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit() {
+    this.getProjectFeatures();
+  }
+
+  getProjectFeatures(): void {
+    this.projectService.getProjectFeatures()
+      .subscribe(projects => {
+        let index: number = 1;
+        for (let i = 0; i < projects.length; i++) {
+          if (projects[i].state === 'show') {
+            index += i;
+          }
+          break;
+        }
+        this.project = index;
+        console.log(index);
+      });
   }
 
 }
