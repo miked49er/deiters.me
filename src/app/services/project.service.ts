159ae591bdc9ed0eff36a0ac64317ba8def0522d
Mike Deiters <mike@deiters.me>
2018-06-24 16:57:34 -0400

    Changed project data service to utilize behaviorsubjects

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,61 +1,81 @@
 import { Injectable } from '@angular/core';
 import { Project } from '../interfaces/project';
 import { PROJECTS, MORE_PROJECTS, MORE_INFO } from '../data/projects';
-import { Observable, of } from 'rxjs';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  projectFeatures: Project[];
+  projectFeatures: BehaviorSubject<Project[]>;
 
   constructor() {
-    this.projectFeatures = PROJECTS.slice(0, 5);
-    this.projectFeatures.push(MORE_PROJECTS);
+    this.projectFeatures = new BehaviorSubject<Project[]>([]);
+    let newArr: Project[] = PROJECTS.slice(0, 5);
+    newArr.push(MORE_PROJECTS);
+    this.projectFeatures.next(newArr);
   }
 
   getProjectFeatures(): Observable<Project[]> {
-    return of(this.projectFeatures);
+    return this.projectFeatures.asObservable();
   }
 
   getProjects(): Observable<Project[]> {
-    return of(this.projectFeatures);
+    return this.projectFeatures.asObservable();
+  }
+
+  getProjectIndex(): Observable<number> {
+    let project: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+    this.getProjectFeatures().subscribe(projects => {
+      let index: number = 1;
+      for (let i = 0; i < projects.length; i++) {
+        if (projects[i].state === 'show') {
+          index += i;
+          break;
+        }
+      }
+      project.next(index);
+    });
+    return project.asObservable();
   }
 
   showNext(index: number): void {
     this.hideProject(index);
     this.showProject(index + 1);
+    this.emitProject();
   }
 
   showPrev(index: number): void {
     this.hideProject(index);
     this.showProject(index - 1);
+    this.emitProject();
   }
 
   showProject(index: number): void {
-    this.projectFeatures[index].state = 'show';
+    this.projectFeatures.getValue()[index].state = 'show';
   }
 
   hideProject(index: number): void {
-    this.projectFeatures[index].state = 'hide';
+    this.projectFeatures.getValue()[index].state = 'hide';
   }
 
   changeProjectTo(id: number): void {
-    for (let i = 0; i < this.projectFeatures.length; i++) {
-      if (this.projectFeatures[i].id === id) {
+    for (let i = 0; i < this.projectFeatures.getValue().length; i++) {
+      if (this.projectFeatures.getValue()[i].id === id) {
         this.showProject(i);
       }
       else {
         this.hideProject(i);
       }
     }
+    this.emitProject();
   }
 
   changeProject(next: boolean): void {
-    for (let i = 0; i < this.projectFeatures.length; i++) {
-      if (this.projectFeatures[i].state === 'show') {
-        if (next && i < this.projectFeatures.length - 1) {
+    for (let i = 0; i < this.projectFeatures.getValue().length; i++) {
+      if (this.projectFeatures.getValue()[i].state === 'show') {
+        if (next && i < this.projectFeatures.getValue().length - 1) {
           this.showNext(i);
         }
         else if (!next && i > 0){
@@ -64,6 +84,10 @@ export class ProjectService {
         break;
       }
     }
+  }
+
+  emitProject(): void {
+    this.projectFeatures.next(this.projectFeatures.getValue());
   }
 
   getMoreInfo(): string {
