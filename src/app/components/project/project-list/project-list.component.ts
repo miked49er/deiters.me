81a0c78e963450a36b0e348ff5d7a90c97cabcac
Mike Deiters <mike@deiters.me>
2018-10-04 19:54:03 -0400

    Removed code unrelated to the project list

--- src/app/components/project/project-list/project-list.component.ts
+++ src/app/components/project/project-list/project-list.component.ts
@@ -10,47 +10,6 @@ import { takeUntil } from 'rxjs/operators';
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
   animations: [
-    trigger('scrollListOverlay', [
-      state('show', style({
-        transform: "translateY(0)"
-      })),
-      state('reveal', style({
-        "clip-path": "polygon(70% -40%, 100% -24%, 100% 100%, 0 100%, 0 0)"
-      })),
-      state('hide', style({
-        transform: "translateY(100%)",
-        display: "none"
-      })),
-      transition('hide => show', animate('400ms ease-in')),
-      transition('hide => reveal', animate('400ms ease-in')),
-      transition('show => reveal', animate('400ms ease-in')),
-      transition('reveal => show', animate('400ms ease-in')),
-      transition('show => hide', animate('400ms ease-out')),
-      transition('reveal => hide', animate('400ms ease-out'))
-    ]),
-    trigger('scrollListTitle', [
-      state('show', style({
-        transform: "translateX(0)",
-        opacity: 1,
-        display: "block"
-      })),
-      state('reveal', style({
-        transform: "translateX(10%)",
-        opacity: 0,
-        display: "none"
-      })),
-      state('hide', style({
-        transform: "translateX(10%)",
-        opacity: 0,
-        display: "none"
-      })),
-      transition('hide => show', animate('400ms 400ms ease-in')),
-      transition('hide => reveal', animate('400ms ease-in')),
-      transition('show => reveal', animate('400ms ease-in')),
-      transition('reveal => show', animate('400ms ease-in')),
-      transition('show => hide', animate('400ms ease-out')),
-      transition('reveal => hide', animate('400ms ease-out'))
-    ]),
     trigger('scrollProjects', [
       state('show', style({
         transform: 'translateY(0)',
@@ -68,23 +27,17 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class ProjectListComponent implements OnInit, OnDestroy {
 
-  primary: boolean = false;
   projects: Project[];
-  backgroundImage: string = 'assets/img/projects-bg.jpg';
-  projectTitle: string;
-  projectView: string = 'hide';
-  slideViewTimer: any;
   projectViewTimer: any;
   destroySubject$: Subject<void> = new Subject();
 
-  @Input() slide: string = 'hide';
+  @Input() projectView: string = 'hide';
 
   constructor (
     private projectService: ProjectService
   ) { }
 
   ngOnInit() {
-    this.slideViewTimer = setTimeout(() => this.slide = 'show', 100);
     this.getProjects();
   }
 
@@ -97,16 +50,13 @@ export class ProjectListComponent implements OnInit, OnDestroy {
     this.projectService.getProjects()
       .pipe(takeUntil(this.destroySubject$))
       .subscribe(projects => this.projects = projects);
-    this.projectTitle = this.projectService.getProjectsTitle();
   }
 
   revealProjects() {
-    this.slide = 'reveal';
     this.projectViewTimer = setTimeout(() => this.projectView = 'show', 500);
   }
 
   hideProjects() {
-    this.slide = 'show';
     this.projectView = 'hide';
   }
 }
