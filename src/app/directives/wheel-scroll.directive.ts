6566ebb3644d913b595b4b11744cc47a32908cf4
Mike Deiters <mike@deiters.me>
2018-10-11 22:28:58 -0400

    Built bottomUp output event and implemented both bottomUp and bottomDown in the pan event. Also reset bottomPosition on active hide.

--- src/app/directives/wheel-scroll.directive.ts
+++ src/app/directives/wheel-scroll.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, Input, HostListener, Inject, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
+import { Debounce } from 'src/app/decorators/debounce';
 
 @Directive({
   selector: '[deitersWheelScroll]'
@@ -15,11 +16,15 @@ export class WheelScrollDirective {
   @Output() top = new EventEmitter();
   @Output() bottom = new EventEmitter();
   @Output() bottomUp = new EventEmitter();
+  @Output() bottomDown = new EventEmitter();
 
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
   @HostListener('wheel', ['$event'])
   checkScroll(e) {
+    if (this.active === 'hide') {
+      this.bottomPosition = -1;
+    }
     if (e.deltaY > 0 && this.active === this.inactiveState) {
       e.preventDefault();
       this.next.emit();
@@ -29,8 +34,11 @@ export class WheelScrollDirective {
       if (e.deltaY < 0 && this.scrollPosition <= 0) {
         this.top.emit();
       }
-      else if (e.deltaY < 0 && this.scrollPosition <= this.bottomPosition + 50) {
+      else if (e.deltaY < 0 && this.scrollPosition <= this.bottomPosition + 100) {
         this.bottomUp.emit();
+      }
+      else if (e.deltaY > 0 && this.scrollPosition >= this.bottomPosition - this.document.documentElement.clientHeight && this.bottomPosition != -1) {
+        this.bottomDown.emit();
       }
       else if (e.deltaY > 0 && bottom) {
         if (this.bottomPosition == -1) {
@@ -43,11 +51,17 @@ export class WheelScrollDirective {
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e) {
+    if (this.active === 'hide') {
+      this.bottomPosition = -1;
+    }
     this.scrollPosition = e.pageY || this.document.documentElement.scrollTop;
   }
 
   @HostListener('pan', ['$event'])
   checkPan(e) {
+    if (this.active === 'hide') {
+      this.bottomPosition = -1;
+    }
     if (e.additionalEvent === 'panup' && this.active === this.inactiveState) {
       this.next.emit();
     }
@@ -55,6 +69,12 @@ export class WheelScrollDirective {
       let bottom = document.documentElement.scrollTop + document.documentElement.clientHeight == document.documentElement.scrollHeight;
       if (e.additionalEvent === 'pandown' && this.scrollPosition <= 0) {
         this.top.emit();
+      }
+      else if (e.additionalEvent === 'pandown' && this.scrollPosition <= this.bottomPosition + 100) {
+        this.bottomUp.emit();
+      }
+      else if (e.additionalEvent === 'panup' && this.scrollPosition >= this.bottomPosition - this.document.documentElement.clientHeight && this.bottomPosition != -1) {
+        this.bottomDown.emit();
       }
       else if (e.additionalEvent === 'panup' && bottom) {
         this.bottom.emit();
